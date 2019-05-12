7795bc5f3eac22b11f83701127c33b56e2d6056d
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-05-12 09:22:02 -0300

    connect with github api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Form } from './styles';
 import logo from '../../assets/images/logo.png';
 import CompareList from '../../components/CompareList';
+import { api } from '../../services/api';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="github compare" />
+class Main extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+  };
 
-    <Form>
-      <input type="text" placeholder="Digite um usuário ou repositório" />
+  handleRepositoryInput = (event) => {
+    this.setState({ repositoryInput: event.target.value });
+  };
 
-      <button type="submit">Buscar</button>
-    </Form>
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { repositoryInput, repositories } = this.state;
+      const response = await api.get(`/repos/${repositoryInput}`);
+      this.setState({
+        repositories: [...repositories, response.data],
+        repositoryInput: '',
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
-    <CompareList />
-  </Container>
-);
+  render() {
+    const { repositories, repositoryInput } = this.state;
+
+    return (
+      <Container>
+        <img src={logo} alt="github compare" />
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Digite um usuário ou repositório"
+            value={repositoryInput}
+            onChange={this.handleRepositoryInput}
+          />
+
+          <button type="submit">Buscar</button>
+        </Form>
+
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
 
 export default Main;
